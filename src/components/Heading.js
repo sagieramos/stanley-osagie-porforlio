34bcdf1f4099b4bb93a6a238419450997f4f1508
Stanley Osagie Ramos <sagiecyber@gmail.com>
2023-12-03 19:10:34 +0100

    title components design

--- src/components/Heading.js
+++ src/components/Heading.js
@@ -1,12 +1,23 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import './heading.css';
+import './heading.scss';
+import { color } from '../signal.ts';
 
-const Heading = ({ text }) => (
-  <div className="heading-i-sagie">
-    {text}
-  </div>
-);
+const Heading = ({ text }) => {
+  const firstCharColor = '#f6b846';
+  const firstChar = text.charAt(0);
+  const displayMode = color.value ? 'title-i-dark-mode' : 'title-i-light-mode';
+
+  return (
+    <div className={`title-i-sagie ${displayMode}`}>
+      <h2>
+        <span style={{ color: firstCharColor }}>{firstChar}</span>
+        {text.slice(1)}
+      </h2>
+      <div />
+    </div>
+  );
+};
 
 Heading.propTypes = {
   text: PropTypes.string,
