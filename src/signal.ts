1418ed02d0f99d3a291354a0b4b091014e2ddb2d
Stanley Osagie Ramos <sagiecyber@gmail.com>
2023-12-03 12:23:16 +0100

    styles

--- src/signal.ts
+++ src/signal.ts
@@ -6,7 +6,7 @@ type SignalBoolean = ReturnType<typeof signal<boolean>>;
 const name: SignalNumber = signal<number>(0);
 const color: SignalBoolean = signal<boolean>(false);
 
-const colorToggle = (bool: boolean): string => (bool ? '#222222' : '#fffaf0');
+const colorToggle = (bool: boolean): string => (bool ? '#222222' : '#fff');
 
 const toggleColor = () => {
   color.value = !color.value;
