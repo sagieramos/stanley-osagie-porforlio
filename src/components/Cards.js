1ae9de53520d9a202d81e64cad8c93d386b02c0f
Stanley Osagie Ramos <sagiecyber@gmail.com>
2023-12-03 22:35:11 +0100

    _

--- src/components/Cards.js
+++ src/components/Cards.js
@@ -1,10 +1,18 @@
 import React from 'react';
 import Aboutme from './Aboutme';
+import './cards.scss';
+import { colorToggle, color } from '../signal.ts';
 
-const Cards = () => (
-  <div className="cards-i-sagie">
-    <Aboutme />
-  </div>
-);
+const Cards = () => {
+  const style = {
+    color: colorToggle(!color.value),
+    background: colorToggle(color.value),
+  };
+  return (
+    <div style={style} className="cards-i-sagie">
+      <Aboutme />
+    </div>
+  );
+};
 
 export default Cards;
